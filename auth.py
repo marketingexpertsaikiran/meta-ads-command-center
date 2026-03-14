2e6013f4daad8007426f8f09b8b1c740b18f4d8e
marketingexpertsaikiran <marketingexpertsaikiran@gmail.com>
2026-03-14 19:38:40 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,51 +1,44 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit as st
 
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 def login():
 
-    st.sidebar.title("Login")
+    st.sidebar.title("User Login")
 
     email = st.sidebar.text_input("Email")
     password = st.sidebar.text_input("Password", type="password")
 
     if st.sidebar.button("Login"):
 
-        user_ref = db.collection("users").document(email)
-        user = user_ref.get()
+        users = db.collection("users").where("email","==",email).get()
 
-        if user.exists:
-            st.session_state["user"] = email
-            st.success("Logged in")
-        else:
-            st.error("User not found")
+        for u in users:
+            if u.to_dict()["password"] == password:
+                st.session_state["user"] = email
+                return True
+
+        st.sidebar.error("Invalid Login")
+
+    return False
 
 
-def save_account(account_id, token):
-
-    user = st.session_state["user"]
+def save_account(user,account):
 
     db.collection("accounts").add({
-        "user": user,
-        "account_id": account_id,
-        "token": token
+        "user":user,
+        "account":account
     })
 
 
-def get_accounts():
+def get_accounts(user):
 
-    user = st.session_state["user"]
+    docs=db.collection("accounts").where("user","==",user).get()
 
-    docs = db.collection("accounts").where("user", "==", user).stream()
-
-    accounts = []
-
-    for doc in docs:
-        accounts.append(doc.to_dict())
-
-    return accounts
+    return [d.to_dict()["account"] for d in docs]
