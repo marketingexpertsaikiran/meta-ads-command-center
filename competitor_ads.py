a2fa5e0e1f40765cb8c052eb111b8c656642b360
marketingexpertsaikiran <marketingexpertsaikiran@gmail.com>
2026-03-15 13:39:28 +0530

    Update competitor_ads.py

--- competitor_ads.py
+++ competitor_ads.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_competitor_ads(keyword):
+def find_ads(keyword):
 
     url = "https://graph.facebook.com/v19.0/ads_archive"
 
@@ -11,16 +11,6 @@ def get_competitor_ads(keyword):
         "limit":10
     }
 
-    response = requests.get(url,params=params).json()
+    res = requests.get(url,params=params).json()
 
-    ads = []
-
-    for ad in response.get("data",[]):
-
-        ads.append({
-            "Page":ad.get("page_name"),
-            "Ad Text":ad.get("ad_creative_body"),
-            "Start Date":ad.get("ad_delivery_start_time")
-        })
-
-    return ads
+    return res.get("data",[])
